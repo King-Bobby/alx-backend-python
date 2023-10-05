d47e6afa27825fc400d2a26b22ec64df86106b8c
Noel Brown <noelbrowndboss@gmail.com>
2023-10-05 20:09:44 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Function returns the floor of a float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of a float"""
+    return math.floor(n)
