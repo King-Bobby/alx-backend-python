38ef05eef1456a622df66cc44d5c6e043b2b7cbb
Noel Brown <noelbrowndboss@gmail.com>
2023-10-09 22:59:23 +0000

    random

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Contains the function wait_n
+"""
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n:int, max_delay:int) -> List[float]:
+    """Use the function wait_random n number of times"""
+    delay = []
+    task = [wait_random(max_delay) for _ in range(n)]
+    delay = await asyncio.gather(*task)
+    return delay
