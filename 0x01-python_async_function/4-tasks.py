692e6f1f2e8afae52bcdc63b07ea78edb7c2804e
Noel Brown <noelbrowndboss@gmail.com>
2023-10-13 22:04:56 +0000

    pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,8 +12,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """updates version of function wait_n"""
+    """This function is an updated version of function wait_n"""
     delays = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
