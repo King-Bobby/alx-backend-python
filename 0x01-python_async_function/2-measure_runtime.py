25e5666186191dc43cce5208e6cf243e882433eb
Noel Brown <noelbrowndboss@gmail.com>
2023-10-13 21:59:13 +0000

    pycodestyle

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-contains the function measure_time
+This module contains the function measure_time
 """
 
 
@@ -12,6 +12,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
+    """
+    measures the total execution time for wait_n(n, max_delay),
+    and returns total_time / n
+    """
     start_time = time.time()
     await wait_n(n, max_delay)
     end_time = time.time()
